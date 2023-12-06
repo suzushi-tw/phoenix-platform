976ccf022e64bf042626d4ba00f8fcc2af6ed339
suzushi-tw <ianhuang2023a@gmail.com>
2023-12-06 09:28:29 +0100

    minor changes to look and add chinese to username

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,37 +13,34 @@ export default async function Home() {
 
   return (
     <>
-      <h1 className="font-bold text-3xl md:text-4xl">Your feed</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
+      <h1 className='font-bold text-3xl md:text-4xl'>Your feed</h1>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6'>
         {/* @ts-expect-error server component */}
         {session ? <CustomFeed /> : <GeneralFeed />}
 
         {/* subreddit info */}
-        <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
-          <div className="bg-emerald-100 px-6 py-4">
-            <p className="font-semibold py-3 flex items-center gap-1.5">
-              <HomeIcon className="w-4 h-4" />
-              Home
+        <div className='overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last'>
+          <div className='bg-red-200 px-6 py-4'>
+            <p className='font-semibold py-3 flex items-center gap-1.5'>
+              <HomeIcon className='h-4 w-4' />
+              看板
             </p>
           </div>
-
-          <div className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
-            <div className="flex justify-between gap-x-4 py-3">
-              <p className="text-zinc-500">
-                Your personal Breadit homepage. Come here to check in with your
-                favorite communities.
+          <dl className='-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6'>
+            <div className='flex justify-between gap-x-4 py-3'>
+              <p className='text-zinc-500'>
+                歡迎回來
               </p>
             </div>
 
             <Link
               className={buttonVariants({
-                className: "w-full mt-4 mb-6 ",
+                className: 'w-full mt-4 mb-6',
               })}
-              href="/r/create"
-            >
-              Create Community
+              href={`/r/create`}>
+              創建看板
             </Link>
-          </div>
+          </dl>
         </div>
       </div>
     </>
--- src/lib/validators/username.ts
+++ src/lib/validators/username.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const UsernameValidator = z.object({
-    name: z.string().min(3).max(32).regex(/^[a-zA-Z0-9_]+$/),
+    name: z.string().min(3).max(32).regex(/^[\u4e00-\u9fa5_a-za-z0-9]+$/),
 })
 
 export type UsernameRequest = z.infer<typeof UsernameValidator>
