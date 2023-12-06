58c64582d3b81b108d6e60459599a4903a7156c4
suzushi-tw <ianhuang2023a@gmail.com>
2023-12-06 09:32:18 +0100

    changes navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,31 @@
 import Link from "next/link";
-import { Icons } from "./Icons";
+// import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
 import { getAuthSession } from "@/lib/auth";
 import UserAccountNav from "./UserAccountNav";
 import SearchBar from "./SearchBar";
+import Image from 'next/image';
 
 const Navbar = async () => {
   const session = await getAuthSession();
   return (
-    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zink-300 z-[10] py-2">
-      <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
-        {/* Logo */}
-        <Link href="/" className="flex gap-2 items-center">
-          <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
-          <p className="hidden text-zinc-700 text-sm font-medium md:block">
-            Breadit
-          </p>
+    <div className='fixed top-0 inset-x-0 h-fit bg-red-200 border-b border-zinc-300 z-[10] py-2'>
+      <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
+        {/* logo */}
+        <Link href='/' className='flex gap-2 items-center'>
+          {/* <Icons.logo className='h-8 w-8 sm:h-6 sm:w-6' /> */}
+          <Image src='/Phoenix_logo.jpg' alt='logo' width={60} height={60} />
+          <p className='hidden text-zinc-700 text-xl font-medium md:block'>Phoenix</p>
         </Link>
-        {/* Search bar */}
+
+        {/* search bar */}
         <SearchBar />
+
+        {/* actions */}
         {session?.user ? (
-          <UserAccountNav user={session.user}/>
+          <UserAccountNav user={session.user} />
         ) : (
-          <Link href="/sign-in" className={buttonVariants()}>
+          <Link href='/sign-in' className={buttonVariants()}>
             Sign In
           </Link>
         )}
